2ff3209d734fb09b13688e59605b7ad0e7b4aeaa
objhidden <danildudaduda@gmail.com>
2023-05-31 15:21:14 +0300

    add login, update password and delete account routes for user

--- routes/user_route.py
+++ routes/user_route.py
@@ -30,3 +30,48 @@ def register_user():
     client = User(first_name, last_name, email, password, client_id)
     access_token = create_access_token(identity=str(client.id))
     return jsonify({'access_token': access_token}), 200
+
+
+@app.route('/api/user/login', methods=['POST'])
+def login_user():
+    data = request.get_json()
+    email = data['email']
+    password = data['password']
+
+    existing_user = mongo.db.user.find_one({'email': email})
+    if not existing_user:
+        return jsonify({'message': 'User not found'}), 404
+
+    if check_password_hash(existing_user['password'], password):
+        access_token = create_access_token(identity=str(existing_user['_id']))
+        return jsonify({'access_token': access_token}), 200
+    else:
+        return jsonify({'message': 'Invalid password'}), 401
+
+
+@app.route('/api/user/update_password', methods=['POST'])
+@jwt_required()
+def update_password():
+    data = request.get_json()
+    new_password = data['new_password']
+
+    current_user_id = get_jwt_identity()
+    existing_user = mongo.db.user.find_one({'_id': ObjectId(current_user_id)})
+    if not existing_user:
+        return jsonify({'message': 'User not found'}), 404
+
+    hashed_password = generate_password_hash(new_password)
+    mongo.db.user.update_one({'_id': ObjectId(current_user_id)}, {'$set': {'password': hashed_password}})
+    return jsonify({'message': 'Password updated successfully'}), 200
+
+
+@app.route('/api/user/delete_account', methods=['DELETE'])
+@jwt_required()
+def delete_account():
+    current_user_id = get_jwt_identity()
+    existing_user = mongo.db.user.find_one({'_id': ObjectId(current_user_id)})
+    if not existing_user:
+        return jsonify({'message': 'User not found'}), 404
+
+    mongo.db.user.delete_one({'_id': ObjectId(current_user_id)})
+    return jsonify({'message': 'Account deleted successfully'}), 200
